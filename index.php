bc864c9cbf510884da13125499a7e96a117965b7
maurorivas2k <maurorivas2k@gmail.com>
2024-03-12 14:43:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
     </head>
     <body>
         <?php echo '<p>"PHP APPTITUDE-APPLOG OVER AZURE APP SERVICE!"</p>'; ?>
+         <?php echo '<p>"runner github node20"</p>'; ?>
     </body>
 </html>
