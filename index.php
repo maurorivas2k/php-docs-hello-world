79981c91989c6e7e169e918af586cd06fd976bb1
maurorivas2k <maurorivas2k@gmail.com>
2024-03-12 13:17:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-
-echo "PHP APPTITUDE-APPLOG OVER AZURE APP SERVICE!";
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>"PHP APPTITUDE-APPLOG OVER AZURE APP SERVICE!"</p>'; ?>
+    </body>
+</html>
