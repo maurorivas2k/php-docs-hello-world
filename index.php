44f91dcfe274907505dfb4b1802b85a845282eb5
maurorivas2k <maurorivas2k@gmail.com>
2024-03-13 07:23:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     </head>
     <body>
         <?php echo '<p>"PHP APPTITUDE-APPLOG OVER AZURE APP SERVICE!"</p>'; ?>
-         <?php echo '<p>"Prueba de pipeline"</p>'; ?>
+        <?php echo '<p>"Prueba de pipeline"</p>'; ?>
+        <?php echo '<p>"Pipeline actualizadoa node20"</p>'; ?>
     </body>
 </html>
